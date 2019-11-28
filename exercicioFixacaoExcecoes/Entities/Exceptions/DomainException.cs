9c16ecc8bbe02a2799925704d13ce449ef3ac165
Livio Assis <livioassis@yahoo.com.br>
2019-11-28 11:40:14 -0300

    first commit

---
+++ exercicioFixacaoExcecoes/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace exercicioFixacaoExcecoes.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ exercicioFixacaoExcecoes/Program.cs
@@ -0,0 +1,48 @@
+﻿using exercicioFixacaoExcecoes.Entities;
+using exercicioFixacaoExcecoes.Entities.Exceptions;
+using System;
+using System.Globalization;
+
+namespace exercicioFixacaoExcecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Withdraw limit: ");
+                double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, holder, balance, limit);
+
+                Console.WriteLine();
+
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account.WithDraw(amount);
+
+
+                Console.WriteLine("New balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+
+        }
+    }
+}
